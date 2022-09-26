051d011fb54d9fc38c65e6e9c6283dffb7f597e6
Patrick Byers <byers@lucasware.com>
2022-09-26 16:50:57 -0400

    first batch script

---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+title This is your first batch script.
+Rem This should be a comment in a batch script. 
+:: This is another way to wrtie a comment.
+set message=Welcome to batch scripting!
+echo %message% 
+pause
